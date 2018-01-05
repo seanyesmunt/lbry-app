581fe2edb60cc8c0eea65971428c9ec094baba11
Alex Liebowitz <alex@alexliebowitz.com>
2018-01-05 01:29:31 -0500

    Convert Windows update alert dialog to native dialog in main process
    Maybe a bit less polished, but I couldn't find a way to prevent the
    main window from closing that works on the current version of Electron,
    and in general it was starting to get too complicated.

--- src/renderer/index.js
+++ src/renderer/index.js
@@ -9,7 +9,7 @@ import lbry from 'lbry';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { doConditionalAuthNavigate, doOpenModal, doDaemonReady, doShowSnackBar, doAutoUpdate } from 'redux/actions/app';
+import { doConditionalAuthNavigate, doDaemonReady, doShowSnackBar, doAutoUpdate } from 'redux/actions/app';
 import { doNavigate } from 'redux/actions/navigation';
 import { doDownloadLanguages } from 'redux/actions/settings';
 import { doUserEmailVerify } from 'redux/actions/user';
